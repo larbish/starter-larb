95cd57738eccc9465e4eec7eb823b09bfb2a593f
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-11-06 12:28:38 +0100

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,71 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+import pkg from './package.json'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+
+  modules: [
+    '@nuxt/ui-pro',
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxthub/core',
+    '@nuxtjs/plausible',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+  ],
+  app: {
+    rootAttrs: {
+      'vaul-drawer-wrapper': '',
+      'class': 'bg-[--ui-bg]',
+    },
+  },
+
+  site: {
+    url: 'https://content3.nuxt.dev',
+  },
+
+  content: {
+    database: {
+      type: 'd1',
+      binding: 'DB',
+    },
+    studio: {
+      enabled: true,
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      version: pkg.version,
+    },
+  },
+
   routeRules: {
-    '/': { prerender: true }
-  }
+    '/': { redirect: '/getting-started', prerender: false },
+  },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-07-09',
+
+  hub: {
+    database: true,
+    cache: true,
+  },
+
+  icon: {
+    clientBundle: {
+      scan: true,
+      includeCustomCollections: true,
+    },
+    provider: 'iconify',
+  },
+
+  image: {
+    provider: 'ipx',
+  },
+  ogImage: {
+    zeroRuntime: true,
+  },
 })
